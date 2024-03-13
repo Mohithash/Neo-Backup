e6d9d1da5fad50d43cdcfe2943b29060b61f96b9
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:12 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/tasks/BackupActionTask.kt
+++ app/src/main/java/com/machiav3lli/backup/tasks/BackupActionTask.kt
@@ -1,20 +1,22 @@
 /*
- * OAndBackupX: open-source apps backup and restore app.
- * Copyright (C) 2020  Antonios Hazim
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+* OAndBackupX: open-source apps backup and restore app.
+* Copyright (C) 2020  Antonios Hazim
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// Import necessary classes and interfaces
 package com.machiav3lli.backup.tasks
 
 import com.machiav3lli.backup.OABX
@@ -25,6 +27,7 @@ import com.machiav3lli.backup.items.ActionResult
 import com.machiav3lli.backup.items.Package
 import kotlin.system.measureTimeMillis
 
+// Defines a class BackupActionTask that extends BaseActionTask
 class BackupActionTask(
     appInfo: Package, oAndBackupX: MainActivityX, shellHandler: ShellHandler, backupMode: Int,
     setInfoBar: (String) -> Unit,
@@ -33,25 +36,31 @@ class BackupActionTask(
     BackupRestoreHelper.ActionType.BACKUP, setInfoBar,
 ) {
 
+    // Override the doInBackground method from the BaseActionTask class
     override fun doInBackground(vararg params: Void?): ActionResult? {
 
+        // Get a reference to MainActivityX and check if it's not null or finishing
         val mainActivityX = mainActivityXReference.get()
         if (mainActivityX == null || mainActivityX.isFinishing) {
             return ActionResult(app, null, "", false)
         }
 
+        // Measure the time taken to execute the backup process
         val time = measureTimeMillis {
 
+            // Set the notification ID and publish progress
             notificationId = System.currentTimeMillis().toInt()
             publishProgress()
 
+            // Call the backup method from BackupRestoreHelper and store the result
             result = BackupRestoreHelper.backup(mainActivityX, null, shellHandler, app, mode)
 
         }
-        OABX.addInfoLogText(
-            "backup: ${app.packageName}: ${(time / 1000 + 0.5).toInt()} sec"
-        )
 
+        // Log the backup time in seconds
+        OABX.addInfoLogText("backup: ${app.packageName}: ${(time / 1000 + 0.5).toInt()} sec")
+
+        // Return the result of the backup process
         return result
     }
 }
