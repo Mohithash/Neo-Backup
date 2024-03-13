b9196ba1d0020df6a807c63b631ae380946c905a
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:48 +0530

    improve

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/ExportedScheduleItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/ExportedScheduleItem.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.weight
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,9 +27,6 @@ import com.machiav3lli.backup.R
 import com.machiav3lli.backup.dbs.entity.Schedule
 import com.machiav3lli.backup.items.StorageFile
 import com.machiav3lli.backup.ui.compose.icons.Phosphor
-import com.machiav3lli.backup.ui.compose.icons.phosphor.ClockCounterClockwise
-import com.machiav3lli.backup.ui.compose.icons.phosphor.TrashSimple
-import java.time.LocalTime
 
 @Composable
 fun ExportedScheduleItem(
@@ -34,7 +35,6 @@ fun ExportedScheduleItem(
     onRestore: (Schedule) -> Unit = { },
     onDelete: (StorageFile) -> Unit = { },
 ) {
-
     ListItem(
         modifier = Modifier
             .fillMaxWidth()
@@ -55,67 +55,4 @@ fun ExportedScheduleItem(
                     overflow = TextOverflow.Ellipsis,
                     maxLines = 1,
                     style = MaterialTheme.typography.titleMedium
-                )
-                ScheduleFilters(item = item)
-            }
-        },
-        supportingContent = {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    var setTime = LocalTime.of(item.timeHour, item.timeMinute).toString()
-                    setTime += " ${
-                        LocalContext.current.resources
-                            .getQuantityString(
-                                R.plurals.sched_interval_formal,
-                                item.interval,
-                                item.interval
-                            )
-                    }"
-                    Text(
-                        text = setTime,
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .weight(1f),
-                        softWrap = true,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1,
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                    ScheduleTypes(item = item)
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    ElevatedActionButton(
-                        icon = Phosphor.TrashSimple,
-                        text = stringResource(id = R.string.delete),
-                        withText = false,
-                        positive = false,
-                        onClick = { onDelete(file) }
-                    )
-
-                    Spacer(modifier = Modifier.weight(1f))
-
-                    ElevatedActionButton(
-                        icon = Phosphor.ClockCounterClockwise,
-                        text = stringResource(id = R.string.dialog_import),
-                        positive = true,
-                        onClick = { onRestore(item) }
-                    )
-                }
-            }
-        },
-    )
-}
-
-@Preview
-@Composable
-fun PreviewExportedScheduleItem() {
-    //ExportedScheduleItem(Schedule())
-}
+              
