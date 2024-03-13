4aca3f5259c68c8b0c214605f0c20efa8238646c
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:39 +0530

    improve

--- app/src/androidTest/kotlin/tests_ui/Test_UI_SelectionSaveLoad.kt
+++ app/src/androidTest/kotlin/tests_ui/Test_UI_SelectionSaveLoad.kt
@@ -1,44 +1,48 @@
-package tests_ui
-
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.click
 import androidx.compose.ui.test.hasTestTag
-import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onChildAt
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performTextInput
-import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.test.printToLog
+import androidx.compose.ui.test.semantics.SemanticsNodeInteraction
+import androidx.compose.ui.test.waitForIdle
 import com.machiav3lli.backup.activities.MainActivityX
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import tests.onNodeWait
-import tests.onNodeWaitOrAssert
 import timber.log.Timber
 
+@ExperimentalCoroutinesApi
 class Test_SelectionSaveLoad {
 
-    @Rule
+    @get:Rule
     @JvmField
     var test: ComposeContentTestRule = createAndroidComposeRule<MainActivityX>()
 
+    private val testCoroutineScope = TestCoroutineScope()
+
     @Before
     fun setUp() {
-        //test.setContent {  }
-        test.onRoot().printToLog("root")
+        Timber.plant(object : Timber.Tree() {
+            override fun d(message: String, vararg args: Any?) {
+                test.printToLog(message)
+            }
+        })
     }
 
     @Test
-    fun test_findList() {
+    fun test_findList() = testCoroutineScope.runBlockingTest {
         test.waitForIdle()
-        val column = test.onNodeWait(hasTestTag("VerticalItemList.Column"), 10000)
+        val column: SemanticsNodeInteraction? = test.onNodeWait(hasTestTag("VerticalItemList.Column"), 10000)
         column?.printToLog("column") ?: Timber.d("----------", "ERROR")
         assert(column != null)
         column?.let {
