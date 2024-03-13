069114fe00e6c5eba26fcfe4c7ef94b41304605f
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:11 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/LinkItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/LinkItem.kt
@@ -7,13 +7,13 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.machiav3lli.backup.ui.item.Link
 
-@Composable
-fun LinkItem(item: Link, onClick: (String) -> Unit) {
+@Composable // Declares this function as a Composable for Jetpack Compose
+fun LinkItem(item: Link, onClick: (String) -> Unit) { // Receives a Link object and a function to handle clicks
     CardSubRow(
-        text = stringResource(id = item.nameId),
-        icon = item.icon,
-        iconColor = colorResource(id = item.iconColorId),
-        modifier = Modifier.fillMaxWidth(),
-        onClick = { onClick(item.uri) }
+        text = stringResource(id = item.nameId), // Sets the text using the nameId from the Link object
+        icon = item.icon, // Sets the icon using the icon from the Link object
+        iconColor = colorResource(id = item.iconColorId), // Sets the icon color using the iconColorId from the Link object
+        modifier = Modifier.fillMaxWidth(), // Makes the row fill the maximum width
+        onClick = { onClick(item.uri) } // Handles clicks by calling the provided onClick function with the uri from the Link object
     )
 }
