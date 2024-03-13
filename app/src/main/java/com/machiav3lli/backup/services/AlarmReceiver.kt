49ff2963bc43b3c7bdebec99cfcf57e82fe2ee31
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:06 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/services/AlarmReceiver.kt
+++ app/src/main/java/com/machiav3lli/backup/services/AlarmReceiver.kt
@@ -15,17 +15,30 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+// AlarmReceiver: A BroadcastReceiver that starts the ScheduleService when triggered.
 package com.machiav3lli.backup.services
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
+// An implementation of BroadcastReceiver that starts the ScheduleService when triggered.
 class AlarmReceiver : BroadcastReceiver() {
+
+    // onReceive: Called when the BroadcastReceiver is triggered.
+    // Extracts the scheduleId from the Intent and starts the ScheduleService.
     override fun onReceive(context: Context?, intent: Intent?) {
+        // Extracts the scheduleId from the Intent, defaulting to -1 if it's not present.
         val scheduleId = intent?.getLongExtra("scheduleId", -1)
+        
+        // Creates an Intent to start the ScheduleService.
         val serviceIntent = Intent(context, ScheduleService::class.java)
+        
+        // Puts the scheduleId into the Intent.
         serviceIntent.putExtra("scheduleId", scheduleId)
+        
+        // Starts the ScheduleService.
         context?.startService(serviceIntent)
     }
 }
