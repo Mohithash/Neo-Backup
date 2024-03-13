fc3d91508676855f104f69a5c008d0db6d0df9b1
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:52 +0530

    comment

--- etc/Phosphor_compose/adjusted/GlobeSimple.kt
+++ etc/Phosphor_compose/adjusted/GlobeSimple.kt
@@ -16,11 +16,23 @@ import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import com.machiav3lli.backup.ui.compose.icons.Phosphor
 
+/**
+ * This file defines the "GlobeSimple" Phosphor icon.
+ * The icon is represented as an ImageVector object, which is created using the Builder class.
+ */
+
+/**
+ * The Phosphor.GlobeSimple property returns the ImageVector object, which is created using the Builder class.
+ * If the ImageVector object has already been created, it is returned from the cache.
+ */
 val Phosphor.GlobeSimple: ImageVector
     get() {
+        // Return the ImageVector object from the cache if it has already been created
         if (_globe_simple != null) {
             return _globe_simple!!
         }
+
+        // Create the ImageVector object using the Builder class
         _globe_simple = Builder(
             name = "Globe-simple",
             defaultWidth = 24.0.dp,
@@ -28,6 +40,7 @@ val Phosphor.GlobeSimple: ImageVector
             viewportWidth = 256.0f,
             viewportHeight = 256.0f,
         ).apply {
+            // Define the path of the vector image using a series of commands
             path(
                 fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
                 strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
@@ -52,38 +65,4 @@ val Phosphor.GlobeSimple: ImageVector
                 close()
                 moveTo(96.3f, 120.0f)
                 curveToRelative(1.8f, -28.8f, 13.3f, -55.7f, 31.7f, -74.4f)
-                curveToRelative(18.4f, 18.7f, 29.9f, 45.6f, 31.7f, 74.4f)
-                close()
-                moveTo(109.7f, 41.9f)
-                curveTo(92.2f, 63.0f, 81.8f, 90.4f, 80.2f, 120.0f)
-                lineTo(40.4f, 120.0f)
-                arcTo(88.2f, 88.2f, 0.0f, false, true, 109.7f, 41.9f)
-                close()
-                moveTo(40.4f, 136.0f)
-                lineTo(80.2f, 136.0f)
-                curveToRelative(1.6f, 29.6f, 12.0f, 57.0f, 29.5f, 78.1f)
-                arcTo(88.2f, 88.2f, 0.0f, false, true, 40.4f, 136.0f)
-                close()
-                moveTo(146.3f, 214.1f)
-                curveToRelative(17.5f, -21.1f, 27.9f, -48.5f, 29.5f, -78.1f)
-                horizontalLineToRelative(39.8f)
-                arcTo(88.2f, 88.2f, 0.0f, false, true, 146.3f, 214.1f)
-                close()
-            }
-        }
-            .build()
-        return _globe_simple!!
-    }
-
-private var _globe_simple: ImageVector? = null
-
-
-
-@Preview
-@Composable
-fun GlobeSimplePreview() {
-    Image(
-        Phosphor.GlobeSimple,
-        null
-    )
-}
+                curveTo
