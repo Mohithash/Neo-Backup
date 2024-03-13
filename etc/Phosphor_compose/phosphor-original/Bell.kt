f71bc3c5d2a966cadd2932483bd5aea0d97aa583
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:54 +0530

    comment

--- etc/Phosphor_compose/phosphor-original/Bell.kt
+++ etc/Phosphor_compose/phosphor-original/Bell.kt
@@ -11,20 +11,22 @@ import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import com.machiav3lli.backup.ui.compose.icons.Phosphor
 
+// Bell Image Vector
 val Phosphor.Bell: ImageVector
     get() {
         if (_bell != null) {
-            return _bell!!
+            return _bell!! // Return the cached ImageVector if it exists
         }
         _bell = Builder(
-            name = "Bell", defaultWidth = 256.0.dp, defaultHeight = 256.0.dp,
+            name = "Bell", defaultWidth = 256.0.dp, defaultHeight = 256.0.dp, // Set the default width and height
             viewportWidth = 256.0f, viewportHeight = 256.0f
         ).apply {
             path(
-                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f, // Set the fill color
                 strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
                 pathFillType = NonZero
             ) {
+                // Path for the bell icon
                 moveTo(220.8f, 175.9f)
                 curveToRelative(-5.9f, -10.2f, -13.0f, -29.6f, -13.0f, -63.9f)
                 verticalLineToRelative(-7.1f)
@@ -40,24 +42,14 @@ val Phosphor.Bell: ImageVector
                 arcToRelative(15.9f, 15.9f, 0.0f, false, false, 13.9f, -8.0f)
                 arcTo(16.2f, 16.2f, 0.0f, false, false, 220.8f, 175.9f)
                 close()
+                // Path for the circle in the bell
                 moveTo(128.0f, 224.0f)
                 arcToRelative(24.1f, 24.1f, 0.0f, false, true, -24.0f, -24.0f)
                 horizontalLineToRelative(48.0f)
                 arcTo(24.1f, 24.1f, 0.0f, false, true, 128.0f, 224.0f)
                 close()
+                // Path for the inner bell shape
                 moveTo(49.0f, 184.0f)
                 curveToRelative(6.9f, -12.0f, 15.2f, -34.1f, 15.2f, -72.0f)
                 verticalLineToRelative(-8.0f)
-                arcTo(63.8f, 63.8f, 0.0f, false, true, 128.0f, 40.0f)
-                horizontalLineToRelative(0.5f)
-                curveToRelative(34.9f, 0.2f, 63.3f, 29.4f, 63.3f, 64.9f)
-                verticalLineTo(112.0f)
-                curveToRelative(0.0f, 37.9f, 8.3f, 60.0f, 15.2f, 72.0f)
-                close()
-            }
-        }
-            .build()
-        return _bell!!
-    }
-
-private var _bell: ImageVector? = null
+                arcTo(63.8f, 63.8f, 0.0f, false, true, 128.0f, 40.0
