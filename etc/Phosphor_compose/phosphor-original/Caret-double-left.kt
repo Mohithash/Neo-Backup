4cb9fe039e08159b782ef1e92c472cd2956f8910
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:56 +0530

    comment

--- etc/Phosphor_compose/phosphor-original/Caret-double-left.kt
+++ etc/Phosphor_compose/phosphor-original/Caret-double-left.kt
@@ -3,23 +3,26 @@ package com.machiav3lli.backup.ui.compose.icons.phosphor
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
-import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.ImageVector.Builder
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 import com.machiav3lli.backup.ui.compose.icons.Phosphor
 
+// Caret-double-left property returns an ImageVector object representing the double left caret icon
 val Phosphor.`Caret-double-left`: ImageVector
     get() {
+        // If the ImageVector object has already been built, return the cached version
         if (`_caret-double-left` != null) {
             return `_caret-double-left`!!
         }
+
+        // Build the ImageVector object using the Builder class
         `_caret-double-left` = Builder(
             name = "Caret-double-left", defaultWidth = 256.0.dp,
             defaultHeight = 256.0.dp, viewportWidth = 256.0f, viewportHeight = 256.0f
         ).apply {
+            // Define the vector path with various commands to create the double left caret shape
             path(
                 fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
                 strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
@@ -34,19 +37,4 @@ val Phosphor.`Caret-double-left`: ImageVector
                 arcToRelative(8.1f, 8.1f, 0.0f, false, true, 11.4f, 11.4f)
                 lineTo(131.3f, 128.0f)
                 close()
-                moveTo(51.3f, 128.0f)
-                lineToRelative(74.4f, -74.3f)
-                arcToRelative(8.1f, 8.1f, 0.0f, false, false, -11.4f, -11.4f)
-                lineToRelative(-80.0f, 80.0f)
-                arcToRelative(8.1f, 8.1f, 0.0f, false, false, 0.0f, 11.4f)
-                lineToRelative(80.0f, 80.0f)
-                arcToRelative(8.2f, 8.2f, 0.0f, false, false, 11.4f, 0.0f)
-                arcToRelative(8.1f, 8.1f, 0.0f, false, false, 0.0f, -11.4f)
-                close()
-            }
-        }
-            .build()
-        return `_caret-double-left`!!
-    }
-
-private var `_caret-double-left`: ImageVector? = null
+                moveTo(51.3
