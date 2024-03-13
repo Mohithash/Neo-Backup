d47232b21b8c9dfd34f8fd749d63333a2f746716
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:58:57 +0530

    comment

--- app/src/androidTest/kotlin/research/Try_systemValues.kt
+++ app/src/androidTest/kotlin/research/Try_systemValues.kt
@@ -7,12 +7,4 @@ import timber.log.Timber
 class Try_systemValues {
 
     @Test
-    fun test_UserHandle() {
-        //val userHandle = android.os.Process.myUserHandle()
-        //val userHandle = android.os.UserHandle.getUserHandleForUid(android.os.Process.myUid())
-        val userHandle = android.os.UserHandle.getUserHandleForUid(0)
-        Timber.i("$userHandle")
-        Timber.i("fields:\n${userHandle.javaClass.fields.joinToString("\n")}")
-        Timber.i("methods:\n${userHandle.javaClass.methods.joinToString("\n")}")
-    }
-}
+
