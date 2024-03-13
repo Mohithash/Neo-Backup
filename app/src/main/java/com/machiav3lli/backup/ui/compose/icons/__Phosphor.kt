c765e97e69211319515d47931edce4b69123aba4
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:03 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/ui/compose/icons/__Phosphor.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/icons/__Phosphor.kt
@@ -1,3 +1,8 @@
+// Package for the UI components of the backup application
 package com.machiav3lli.backup.ui.compose.icons
 
-object Phosphor
+// Object containing Phosphor icons
+object Phosphor {
+    // Add more detailed comments here about what this object is for and how it is used in the code
+}
+
