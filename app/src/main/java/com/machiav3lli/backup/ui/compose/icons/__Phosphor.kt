8a29bcd135f3d5acb0683926325449a374298514
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:33 +0530

    improve

--- app/src/main/java/com/machiav3lli/backup/ui/compose/icons/__Phosphor.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/icons/__Phosphor.kt
@@ -1,8 +1,13 @@
 // Package for the UI components of the backup application
 package com.machiav3lli.backup.ui.compose.icons
 
-// Object containing Phosphor icons
+/**
+ * Object containing Phosphor icons.
+ *
+ * This object provides a centralized place to store Phosphor icons used in the UI components of the backup application.
+ */
 object Phosphor {
-    // Add more detailed comments here about what this object is for and how it is used in the code
+    // Add Phosphor icons here as constants
+    const val ICON_NAME = "icon_path"
 }
 
