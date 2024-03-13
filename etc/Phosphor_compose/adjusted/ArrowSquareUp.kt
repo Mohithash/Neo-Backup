67fd3a79cb615aa73e3bafb772378288d30dae20
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:46 +0530

    comment

--- etc/Phosphor_compose/adjusted/ArrowSquareUp.kt
+++ etc/Phosphor_compose/adjusted/ArrowSquareUp.kt
@@ -1,38 +1,34 @@
 package com.machiav3lli.backup.ui.compose.icons.phosphor
 
+// `Phosphor` class provides a set of icons for the application
 
-import androidx.compose.foundation.Image
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
-import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.ImageVector.Builder
-import androidx.compose.ui.graphics.vector.path
-import androidx.compose.ui.unit.dp
-import com.machiav3lli.backup.ui.compose.icons.Phosphor
-
+// Custom ImageVector object named ArrowSquareUp
 val Phosphor.ArrowSquareUp: ImageVector
     get() {
+        // If the ImageVector object has already been created, return it
         if (_arrow_square_up != null) {
             return _arrow_square_up!!
         }
+
+        // Create a new ImageVector object using the Builder class
         _arrow_square_up = Builder(
-            name = "Arrow-square-up",
-            defaultWidth = 24.0.dp,
-            defaultHeight = 24.0.dp,
-            viewportWidth = 256.0f,
-            viewportHeight = 256.0f,
+            name = "Arrow-square-up", // Name of the ImageVector object
+            defaultWidth = 24.0.dp, // Default width of the object
+            defaultHeight = 24.0.dp, // Default height of the object
+            viewportWidth = 256.0f, // Width of the viewport
+            viewportHeight = 256.0f // Height of the viewport
         ).apply {
+            // Define the vector path using the path() function
             path(
-                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
-                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
-                pathFillType = NonZero
+                fill = SolidColor(Color(0xFF000000)), // Fill color of the path
+                stroke = null, // No stroke for the path
+                strokeLineWidth = 0.0f, // Width of the stroke
+                strokeLineCap = Butt, // Cap style of the stroke
+                strokeLineJoin = Miter, // Join style of the stroke
+                strokeLineMiter = 4.0f, // Miter limit of the stroke
+                pathFillType = NonZero // Path fill type
             ) {
+                // Define the path using lines, arcs, and other vector operations
                 moveTo(208.0f, 32.0f)
                 lineTo(48.0f, 32.0f)
                 arcTo(16.0f, 16.0f, 0.0f, false, false, 32.0f, 48.0f)
@@ -56,28 +52,4 @@ val Phosphor.ArrowSquareUp: ImageVector
                 arcToRelative(8.0f, 8.0f, 0.0f, false, true, 0.0f, 11.3f)
                 arcToRelative(7.8f, 7.8f, 0.0f, false, true, -5.7f, 2.3f)
                 arcToRelative(7.6f, 7.6f, 0.0f, false, true, -5.6f, -2.3f)
-                lineTo(136.0f, 107.3f)
-                lineTo(136.0f, 168.0f)
-                arcToRelative(8.0f, 8.0f, 0.0f, false, true, -16.0f, 0.0f)
-                lineTo(120.0f, 107.3f)
-                lineTo(99.7f, 127.6f)
-                arcTo(8.0f, 8.0f, 0.0f, false, true, 88.4f, 127.6f)
-                close()
-            }
-        }
-            .build()
-        return _arrow_square_up!!
-    }
-
-private var _arrow_square_up: ImageVector? = null
-
-
-
-@Preview
-@Composable
-fun ArrowSquareUpPreview() {
-    Image(
-        Phosphor.ArrowSquareUp,
-        null
-    )
-}
+                lineTo(136.0f
