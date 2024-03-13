1e41572a660efbdc6d1f56727bc1d4d4cf29233f
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:10 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/preferences/ui/PrefBuilder.kt
+++ app/src/main/java/com/machiav3lli/backup/preferences/ui/PrefBuilder.kt
@@ -17,64 +17,64 @@ import com.machiav3lli.backup.ui.item.PasswordPref
 import com.machiav3lli.backup.ui.item.Pref
 import com.machiav3lli.backup.ui.item.StringPref
 
-@Composable
+@Composable // Indicates that this function can be used in a Composable context
 fun PrefsBuilder(
-    pref: Pref,
-    onDialogPref: (Pref) -> Unit,
-    index: Int,
-    size: Int,
+    pref: Pref, // The preference object to be displayed
+    onDialogPref: (Pref) -> Unit, // Function to be called when a preference dialog is opened
+    index: Int, // The index of the current preference in the list
+    size: Int, // The total number of preferences in the list
 ) {
-    when (pref) {
+    when (pref) { // Switch statement to handle different types of preferences
 
         // order from derived to base classes (otherwise base would obscure derived)
 
-        is LaunchPref   -> LaunchPreference(
+        is LaunchPref -> LaunchPreference( // Display a LaunchPreference
             pref = pref,
-            summary = pref.summary,
+            summary = pref.summary, // Use the summary provided with the preference
             index = index,
             groupSize = size,
             onClick = pref.onClick
         )
 
-        is EnumPref     -> EnumPreference(
+        is EnumPref -> EnumPreference( // Display an EnumPreference
             pref = pref,
             index = index,
             groupSize = size,
         ) {
-            onDialogPref(pref)
+            onDialogPref(pref) // Call onDialogPref when the dialog is opened
         }
 
-        is ListPref     -> ListPreference(
+        is ListPref -> ListPreference( // Display a ListPreference
             pref = pref,
             index = index,
             groupSize = size,
         ) {
-            onDialogPref(pref)
+            onDialogPref(pref) // Call onDialogPref when the dialog is opened
         }
 
-        is PasswordPref -> PasswordPreference(
+        is PasswordPref -> PasswordPreference( // Display a PasswordPreference
             pref = pref,
             index = index,
             groupSize = size,
         ) {
-            onDialogPref(pref)
+            onDialogPref(pref) // Call onDialogPref when the dialog is opened
         }
 
-        is StringPref   -> StringPreference(
+        is StringPref -> StringPreference( // Display a StringPreference
             pref = pref,
             index = index,
             groupSize = size,
         ) {
-            onDialogPref(pref)
+            onDialogPref(pref) // Call onDialogPref when the dialog is opened
         }
 
-        is IntPref      -> IntPreference(
+        is IntPref -> IntPreference( // Display an IntPreference
             pref = pref,
             index = index,
             groupSize = size,
         )
 
-        is BooleanPref  -> BooleanPreference(
+        is BooleanPref -> BooleanPreference( // Display a BooleanPreference
             pref = pref,
             index = index,
             groupSize = size,
