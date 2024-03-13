9843cf8b1cdb5d7d29448fe83bcc1235fd9641e0
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:36 +0530

    improve

--- app/src/main/java/com/machiav3lli/backup/dbs/dao/AppInfoDao.kt
+++ app/src/main/java/com/machiav3lli/backup/dbs/dao/AppInfoDao.kt
@@ -8,45 +8,47 @@
 package com.machiav3lli.backup.dbs.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import com.machiav3lli.backup.dbs.entity.AppInfo
 import kotlinx.coroutines.flow.Flow
 
-@Dao // Denotes that this is a Data Access Object for the AppInfo table
+@Dao
 interface AppInfoDao : BaseDao<AppInfo> {
 
     @Query("SELECT COUNT(*) FROM appinfo")
-    // Query to count the number of records in the appinfo table
-    fun count(): Long // Returns the count as a Long
+    fun count(): Long
 
     @Query("SELECT * FROM appinfo ORDER BY packageName ASC")
-    // Query to select all records from the appinfo table, ordered by packageName in ascending order
-    fun getAll(): MutableList<AppInfo> // Returns a mutable list of AppInfo objects
+    fun getAll(): List<AppInfo>
 
     @Query("SELECT * FROM appinfo ORDER BY packageName ASC")
-    // Query to select all records from the appinfo table, ordered by packageName in ascending order
-    // This function returns a Flow, which is a reactive data holder that allows observing data changes
-    fun getAllFlow(): Flow<MutableList<AppInfo>>
+    fun getAllFlow(): Flow<List<AppInfo>>
 
     @Query("SELECT * FROM appinfo WHERE packageName = :packageName")
-    // Query to select a single record from the appinfo table where packageName matches the provided packageName
-    fun get(packageName: String): AppInfo // Returns the AppInfo object
+    fun get(packageName: String): AppInfo?
 
-    @Query("DELETE FROM appinfo")
-    // Query to delete all records from the appinfo table
-    fun emptyTable() // Empties the table
+    @Delete
+    fun delete(appInfo: AppInfo)
 
-    @Query("SELECT * FROM appinfo WHERE packageName = :packageName")
-    // Query to select all records from the appinfo table where packageName matches the provided packageName
-    fun deleteAllOf(packageName: String): Int // Returns the number of deleted records
+    @Query("DELETE FROM appinfo WHERE packageName = :packageName")
+    fun deleteAllOf(packageName: String): Int
 
-    @Transaction // Indicates that the following methods should be executed in a single transaction
-    fun updateList(vararg appInfos: AppInfo) {
-        // Empties the appinfo table
-        emptyTable() // Returns the number of deleted records
+    @Insert
+    fun insert(appInfo: AppInfo)
 
-        // Replaces the contents of the appinfo table with the provided AppInfo objects
-        replaceInsert(*appInfos) // Inserts the AppInfo objects into the appinfo table
+    @Insert
+    fun insert(appInfos: List<AppInfo>)
+
+    @Update
+    fun update(appInfo: AppInfo)
+
+    @Transaction
+    fun updateList(appInfos: List<AppInfo>) {
+        deleteAllOf(*appInfos.map { it.packageName }.toTypedArray())
+        insert(appInfos)
     }
 }
