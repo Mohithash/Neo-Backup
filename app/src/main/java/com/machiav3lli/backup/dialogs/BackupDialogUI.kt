b1ca00d0cae20e3433ae83eb9c829788209560b2
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:32 +0530

    improve

--- app/src/main/java/com/machiav3lli/backup/dialogs/BackupDialogUI.kt
+++ app/src/main/java/com/machiav3lli/backup/dialogs/BackupDialogUI.kt
@@ -1,3 +1,32 @@
 /*
- * OAndBackupX: open-source apps backup and restore app.
- * Copyright (C) 2
+ * OAndBackupX: Open-source app backup and restore tool.
+ * Copyright (C) 2023 Your Name
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Include necessary headers here
+
+// Constants and macros go here
+
+// Data structures and types go here
+
+// Function prototypes go here
+
+// Implement functions here
+
+int main() {
+    // Main function implementation goes here
+    return 0;
+}
