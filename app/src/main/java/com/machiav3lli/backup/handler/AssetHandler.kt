b801a0257ccde8e8bfb5a2112e888b6c9752fa00
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:05 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/handler/AssetHandler.kt
+++ app/src/main/java/com/machiav3lli/backup/handler/AssetHandler.kt
@@ -9,18 +9,31 @@ import timber.log.Timber
 import java.io.File
 import java.io.FileOutputStream
 
+/**
+ * Handles asset-related operations. Copies assets to the application's file storage,
+ * manages excluded files and directories, and writes exclude files for backup and restore.
+ */
 class AssetHandler(context: Context) {
 
+    /** The version file name. */
     val VERSION_FILE = "__version__"
+
+    /** The subdirectory name for assets. */
     val ASSETS_SUBDIR = "assets"
 
+    /** The directory where the assets are stored in the application's file storage. */
     var directory: File
         private set
 
+    /**
+     * Initializes the AssetHandler instance. Copies scripts to file storage if the files
+     * don't exist or have a different version than the current app version.
+     */
     init {
         // copy scripts to file storage
         directory = File(context.filesDir, ASSETS_SUBDIR)
         directory.mkdirs()
+
         // don't copy if the files exist and are from the current app version
         val appVersion = BuildConfig.VERSION_NAME
         val version = try {
@@ -42,73 +55,4 @@ class AssetHandler(context: Context) {
         }
     }
 
-    // @hg42 why exclude lib? how is it restored?
-    // @machiav3lli libs are generally created while installing the app. Backing them up
-    // would result a compatibility problem between devices with different cpu_arch
-
-    val DATA_BACKUP_EXCLUDED_BASENAMES get() = listOfNotNull(
-        "lib",      //TODO hg42 what about architecture dependent names? or may be application specific? lib* ???
-        if (!pref_backupNoBackupData.value) "no_backup" else null //TODO hg42 use Context.getNoBackupFilesDir() ??? tricky, because it's an absolute path (remove common part...)
-    )
-
-    val DATA_RESTORE_EXCLUDED_BASENAMES get() = listOfNotNull(
-        "lib",      //TODO hg42 what about architecture dependent names? or may be application specific? lib* ???
-        if (!pref_restoreNoBackupData.value) "no_backup" else null //TODO hg42 use Context.getNoBackupFilesDir() ??? tricky, because it's an absolute path (remove common part...)
-    )
-
-    val DATA_EXCLUDED_CACHE_DIRS get() = listOf(
-        "cache",
-        "code_cache"
-    )
-
-    val DATA_EXCLUDED_NAMES get() = listOfNotNull(
-        "com.google.android.gms.appid.xml",
-        "com.machiav3lli.backup.xml", // encrypted prefs file
-        //"cache",  // don't, this also excludes the cache
-        "trash",
-        ".thumbnails",
-        if (ShellHandler.utilBox.hasBug("DotDotDirHang")) "..*" else null
-    )
-
-    fun updateExcludeFiles() {
-
-        File(directory, ShellHandler.BACKUP_EXCLUDE_FILE)
-            .writeText(
-                (DATA_BACKUP_EXCLUDED_BASENAMES.map { "./$it" }
-                        + DATA_EXCLUDED_NAMES)
-                    .joinToString("") { it + "\n" }
-            )
-        File(directory, ShellHandler.RESTORE_EXCLUDE_FILE)
-            .writeText(
-                (DATA_RESTORE_EXCLUDED_BASENAMES.map { "./$it" }
-                        + DATA_EXCLUDED_NAMES)
-                    .joinToString("") { it + "\n" }
-            )
-        File(directory, ShellHandler.EXCLUDE_CACHE_FILE)
-            .writeText(
-                DATA_EXCLUDED_CACHE_DIRS.map { "./$it" }
-                    .joinToString("") { it + "\n" }
-            )
-    }
-}
-
-fun AssetManager.copyRecursively(assetPath: String, targetFile: File) {
-    list(assetPath)?.let { list ->
-        if (list.isEmpty()) { // assetPath is file
-            open(assetPath).use { input ->
-                FileOutputStream(targetFile.absolutePath).use { output ->
-                    input.copyTo(output)
-                    output.flush()
-                }
-            }
-
-        } else { // assetPath is folder
-            targetFile.deleteRecursively()
-            targetFile.mkdir()
-
-            list.forEach {
-                copyRecursively("$assetPath/$it", File(targetFile, it))
-            }
-        }
-    }
-}
+    // @hg4
