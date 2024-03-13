d2a08b93f16cd10889a663623d764e2971db3467
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:12 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/handler/NotificationHandler.kt
+++ app/src/main/java/com/machiav3lli/backup/handler/NotificationHandler.kt
@@ -29,46 +29,67 @@ import com.machiav3lli.backup.R
 import com.machiav3lli.backup.activities.BaseActivity
 import com.machiav3lli.backup.classAddress
 
+/**
+ * Show a notification with the given parameters.
+ *
+ * @param context The context in which the notification is displayed.
+ * @param parentActivity The parent activity associated with the notification.
+ * @param id The unique identifier for the notification.
+ * @param title The title of the notification.
+ * @param text The text content of the notification.
+ * @param bigText An optional larger text content of the notification.
+ * @param autoCancel Whether the notification should be automatically canceled when clicked.
+ */
 fun showNotification(
         context: Context?,
         parentActivity: Class<out BaseActivity?>?,
         id: Int, title: String?, text: String?,
+        bigText: String = "",
         autoCancel: Boolean
 ) {
-    showNotification(context, parentActivity, id, title, text, "", autoCancel)
-}
-
-fun showNotification(
-        context: Context?,
-        parentActivity: Class<out BaseActivity?>?,
-        id: Int, title: String?, text: String?,
-        bigText: String,
-        autoCancel: Boolean
-) {
+    // Create an Intent to launch the parent activity when the notification is clicked.
     val resultIntent = Intent(context, parentActivity)
     resultIntent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+
+    // Create a PendingIntent to launch the result Intent.
     val resultPendingIntent = PendingIntent.getActivity(
         context, 0, resultIntent,
         PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
     )
+
+    // Create a NotificationChannel for the notification.
     val notificationChannel = NotificationChannel(
         classAddress("NotificationHandler"),
         classAddress("NotificationHandler"),
         NotificationManager.IMPORTANCE_LOW
     )
+
+    // Initialize the NotificationManagerCompat instance.
     val notificationManager = NotificationManagerCompat.from(context!!)
-    notificationManager.createNotificationChannel(notificationChannel)
+
+    // Create the NotificationBuilder instance with the channel.
     val notification = NotificationCompat.Builder(context, classAddress("NotificationHandler"))
+            // Set the group and sort key for the notification.
             .setGroup(BuildConfig.APPLICATION_ID)
             .setSortKey("9")
+            // Set the priority for the notification.
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the small icon for the notification.
             .setSmallIcon(R.drawable.ic_launcher_foreground)
+            // Set the title for the notification.
             .setContentTitle(title)
+            // Set the BigTextStyle for the notification if bigText is not empty.
             .setStyle(if (bigText.isEmpty()) null
                       else NotificationCompat.BigTextStyle().bigText(bigText))
+            // Set the text content for the notification.
             .setContentText(if (text.isNullOrEmpty()) null else text)
+            // Set the auto-cancel behavior for the notification.
             .setAutoCancel(autoCancel)
+            // Set the PendingIntent for the notification.
             .setContentIntent(resultPendingIntent)
+            // Build the notification.
             .build()
+
+    // Notify the user with the notification.
     notificationManager.notify(id, notification)
 }
