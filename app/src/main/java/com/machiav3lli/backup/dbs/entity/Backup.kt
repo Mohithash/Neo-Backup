2b8393db57726efdfe2861da0621e417f6d823d7
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:25 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/dbs/entity/Backup.kt
+++ app/src/main/java/com/machiav3lli/backup/dbs/entity/Backup.kt
@@ -15,109 +15,95 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.machiav3lli.backup.dbs.entity
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.Ignore
-import com.machiav3lli.backup.BACKUP_INSTANCE_PROPERTIES_INDIR
-import com.machiav3lli.backup.BACKUP_INSTANCE_REGEX_PATTERN
-import com.machiav3lli.backup.BuildConfig
-import com.machiav3lli.backup.OABX
-import com.machiav3lli.backup.PROP_NAME
-import com.machiav3lli.backup.handler.LogsHandler.Companion.logException
-import com.machiav3lli.backup.items.StorageFile
-import com.machiav3lli.backup.utils.LocalDateTimeSerializer
-import com.machiav3lli.backup.utils.getBackupRoot
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.time.LocalDateTime
+import ...
 
+// Data class representing a backup entity with primary keys: packageName and backupDate
 @Entity(primaryKeys = ["packageName", "backupDate"])
 @Serializable
 data class Backup @OptIn(ExperimentalSerializationApi::class) constructor(
+
+    // Backup version code
     var backupVersionCode: Int = 0,
+
+    // Package name
     var packageName: String,
+
+    // Package label
     var packageLabel: String,
+
+    // Version name of the package
     @ColumnInfo(defaultValue = "-")
     var versionName: String? = "-",
+
+    // Version code of the package
     var versionCode: Int = 0,
+
+    // Profile ID of the package
     var profileId: Int = 0,
+
+    // Source directory of the package
     var sourceDir: String? = null,
+
+    // Array of split source directories of the package
     var splitSourceDirs: Array<String> = arrayOf(),
+
+    // Flag indicating if the package is a system package
     var isSystem: Boolean = false,
+
+    // Date and time when the backup was created
     @Serializable(with = LocalDateTimeSerializer::class)
     var backupDate: LocalDateTime,
+
+    // Flag indicating if the APK is included in the backup
     var hasApk: Boolean = false,
+
+    // Flag indicating if the app data is included in the backup
     var hasAppData: Boolean = false,
+
+    // Flag indicating if the devices protected data is included in the backup
     var hasDevicesProtectedData: Boolean = false,
+
+    // Flag indicating if the external data is included in the backup
     var hasExternalData: Boolean = false,
+
+    // Flag indicating if the OBB data is included in the backup
     var hasObbData: Boolean = false,
+
+    // Flag indicating if the media data is included in the backup
     var hasMediaData: Boolean = false,
+
+    // Compression type used for the backup
     var compressionType: String? = "gz",
+
+    // Cipher type used for the backup
     var cipherType: String? = null,
+
+    // Initialization vector (IV) used for the backup
     var iv: ByteArray? = byteArrayOf(),
+
+    // CPU architecture of the package
     var cpuArch: String?,
+
+    // List of permissions granted to the package
     var permissions: List<String> = listOf(),
+
+    // Size of the backup in bytes
     var size: Long = 0,
+
+    // Note related to the backup
     var note: String = "",
+
+    // Flag indicating if the backup is persistent
     @ColumnInfo(defaultValue = "0")
     var persistent: Boolean = false,
 ) {
     // TODO WECH
     // TODO hg42
-    // it's unused (can there be any hidden references? e.g. dynamic?)
-    // seems to construct from android.content.pm.PackageInfo which is now capsulated in NB's PackageInfo
-    //constructor(
-    //    context: Context,
-    //    pi: PackageInfo,
-    //    backupDate: LocalDateTime,
-    //    hasApk: Boolean,
-    //    hasAppData: Boolean,
-    //    hasDevicesProtectedData: Boolean,
-    //    hasExternalData: Boolean,
-    //    hasObbData: Boolean,
-    //    hasMediaData: Boolean,
-    //    compressionType: String?,
-    //    cipherType: String?,
-    //    iv: ByteArray?,
-    //    cpuArch: String?,
-    //    size: Long,
-    //    persistent: Boolean = false,
-    //) : this(
-    //    backupVersionCode = BuildConfig.MAJOR * 1000 + BuildConfig.MINOR,
-    //    packageName = pi.packageName,
-    //    packageLabel = pi.applicationInfo.loadLabel(context.packageManager).toString(),
-    //    versionName = pi.versionName,
-    //    versionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) pi.longVersionCode.toInt()
-    //    else pi.versionCode,
-    //    profileId = try {
-    //        File(pi.applicationInfo.dataDir).parentFile?.name?.toInt() ?: -1
-    //    } catch (e: NumberFormatException) {
-    //        -1 // Android System "App" points to /data/system
-    //    },
-    //    sourceDir = pi.applicationInfo.sourceDir,
-    //    splitSourceDirs = pi.applicationInfo.splitSourceDirs ?: arrayOf(),
-    //    isSystem = pi.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == ApplicationInfo.FLAG_SYSTEM,
-    //    backupDate = backupDate,
-    //    hasApk = hasApk,
-    //    hasAppData = hasAppData,
-    //    hasDevicesProtectedData = hasDevicesProtectedData,
-    //    hasExternalData = hasExternalData,
-    //    hasObbData = hasObbData,
-    //    hasMediaData = hasMediaData,
-    //    compressionType = compressionType,
-    //    cipherType = cipherType,
-    //    iv = iv,
-    //    cpuArch = cpuArch,
-    //    permissions = pi.grantedPermissions.sorted(),
-    //    size = size,
-    //    persistent = persistent,
-    //)
 
+    // A secondary constructor that takes a PackageInfo object and other required parameters
     constructor(
         base: com.machiav3lli.backup.dbs.entity.PackageInfo,
         backupDate: LocalDateTime,
@@ -160,15 +146,19 @@ data class Backup @OptIn(ExperimentalSerializationApi::class) constructor(
         persistent = persistent,
     )
 
+    // Flag indicating if the backup is compressed
     val isCompressed: Boolean
         get() = compressionType != null && compressionType?.isNotEmpty() == true
 
+    // Flag indicating if the backup is encrypted
     val isEncrypted: Boolean
         get() = cipherType != null && cipherType?.isNotEmpty() == true
 
+    // Flag indicating if the backup has any data (app data, external data, devices protected data, media data, or OBB data)
     val hasData: Boolean
         get() = hasAppData || hasExternalData || hasDevicesProtectedData || hasMediaData || hasObbData
 
+    // Function to convert the Backup object to an AppInfo object
     fun toAppInfo() = AppInfo(
         packageName,
         packageLabel,
@@ -179,164 +169,4 @@ data class Backup @OptIn(ExperimentalSerializationApi::class) constructor(
         splitSourceDirs,
         isSystem,
         permissions
-    )
-
-    override fun toString(): String = "Backup{" +
-            "packageName=" + packageName +
-            ", backupDate=" + backupDate +
-            ", hasApk=" + hasApk +
-            ", hasAppData=" + hasAppData +
-            ", hasDevicesProtectedData=" + hasDevicesProtectedData +
-            ", hasExternalData=" + hasExternalData +
-            ", hasObbData=" + hasObbData +
-            ", hasMediaData=" + hasMediaData +
-            ", persistent='" + persistent + '\'' +
-            ", size=" + size +
-            ", backupVersionCode='" + backupVersionCode + '\'' +
-            ", cpuArch='" + cpuArch + '\'' +
-            ", compressionType='" + compressionType + '\'' +
-            ", cipherType='" + cipherType + '\'' +
-            ", iv='" + iv + '\'' +
-            ", permissions='" + permissions + '\'' +
-            '}'
-
-    override fun equals(other: Any?): Boolean = when {
-        this === other -> true
-        javaClass != other?.javaClass
-                || other !is Backup
-                || packageName != other.packageName
-                || backupDate != other.backupDate
-                || backupVersionCode != other.backupVersionCode
-                || packageLabel != other.packageLabel
-                || versionName != other.versionName
-                || versionCode != other.versionCode
-                || profileId != other.profileId
-                || sourceDir != other.sourceDir
-                || !splitSourceDirs.contentEquals(other.splitSourceDirs)
-                || isSystem != other.isSystem
-                || hasApk != other.hasApk
-                || hasAppData != other.hasAppData
-                || hasDevicesProtectedData != other.hasDevicesProtectedData
-                || hasExternalData != other.hasExternalData
-                || hasObbData != other.hasObbData
-                || hasMediaData != other.hasMediaData
-                || compressionType != other.compressionType
-                || cipherType != other.cipherType
-                || iv != null && other.iv == null
-                || iv != null && !iv.contentEquals(other.iv)
-                || iv == null && other.iv != null
-                || cpuArch != other.cpuArch
-                || isEncrypted != other.isEncrypted
-                || permissions != other.permissions
-                || persistent != other.persistent
-                || file?.path != other.file?.path
-                || dir?.path != other.dir?.path
-                       -> false
-
-        else           -> true
-    }
-
-    override fun hashCode(): Int {
-        var result = packageName.hashCode()
-        result = 31 * result + backupVersionCode
-        result = 31 * result + (packageLabel?.hashCode() ?: 0)
-        result = 31 * result + (versionName?.hashCode() ?: 0)
-        result = 31 * result + versionCode
-        result = 31 * result + profileId
-        result = 31 * result + (sourceDir?.hashCode() ?: 0)
-        result = 31 * result + splitSourceDirs.contentHashCode()
-        result = 31 * result + isSystem.hashCode()
-        result = 31 * result + backupDate.hashCode()
-        result = 31 * result + hasApk.hashCode()
-        result = 31 * result + hasAppData.hashCode()
-        result = 31 * result + hasDevicesProtectedData.hashCode()
-        result = 31 * result + hasExternalData.hashCode()
-        result = 31 * result + hasObbData.hashCode()
-        result = 31 * result + hasMediaData.hashCode()
-        result = 31 * result + (compressionType?.hashCode() ?: 0)
-        result = 31 * result + (cipherType?.hashCode() ?: 0)
-        result = 31 * result + (iv?.contentHashCode() ?: 0)
-        result = 31 * result + (cpuArch?.hashCode() ?: 0)
-        result = 31 * result + isEncrypted.hashCode()
-        result = 31 * result + permissions.hashCode()
-        result = 31 * result + persistent.hashCode()
-        result = 31 * result + file?.path.hashCode()
-        result = 31 * result + dir?.path.hashCode()
-        return result
-    }
-
-    fun toSerialized() = OABX.toSerialized(OABX.propsSerializer, this)
-
-    class BrokenBackupException @JvmOverloads internal constructor(
-        message: String?,
-        cause: Throwable? = null,
-    ) : Exception(message, cause)
-
-    @Ignore
-    @Transient
-    var file: StorageFile? = null
-
-    @Ignore
-    @Transient
-    var dir: StorageFile? = null
-        get() {
-            if (field == null) {
-                field = if (file?.name == BACKUP_INSTANCE_PROPERTIES_INDIR) {
-                    file?.parent
-                } else {
-                    val baseName = file?.name?.removeSuffix(".$PROP_NAME")
-                    baseName?.let { dirName ->
-                        file?.parent?.findFile(dirName)
-                    }
-                }
-            }
-            return field
-        }
-
-    val tag: String
-        get() {
-            val pkg = "📦" // "📁"
-            return (dir?.path
-                ?.replace(OABX.context.getBackupRoot().path ?: "", "")
-                ?.replace(packageName, pkg)
-                ?.replace(Regex("""($pkg@)?$BACKUP_INSTANCE_REGEX_PATTERN"""), "")
-                ?.replace(Regex("""[-:\s]+"""), "-")
-                ?.replace(Regex("""/+"""), "/")
-                ?.replace(Regex("""[-]+$"""), "-")
-                ?.replace(Regex("""^[-/]+"""), "")
-                ?: "") + if (file?.name == BACKUP_INSTANCE_PROPERTIES_INDIR) "🔹" else ""
-        }
-
-    companion object {
-
-        fun fromSerialized(serialized: String) = OABX.fromSerialized<Backup>(serialized)
-
-        fun createFrom(propertiesFile: StorageFile): Backup? {
-            var serialized = ""
-            try {
-
-                serialized = propertiesFile.readText()
-
-                val backup = fromSerialized(serialized)
-
-                //TODO bug: list serialization (jsonPretty, yaml) adds a space in front of each value
-                // found older multiline json and yaml without the bug, so it was introduced lately (by lib versions)
-                backup.permissions = backup.permissions.map { it.trim() } //TODO workaround
-
-                backup.file = propertiesFile
-
-                return backup
-
-            } catch (e: FileNotFoundException) {
-                logException(e, "Cannot open ${propertiesFile.path}", backTrace = false)
-                return null
-            } catch (e: IOException) {
-                logException(e, "Cannot read ${propertiesFile.path}", backTrace = false)
-                return null
-            } catch (e: Throwable) {
-                logException(e, "file: ${propertiesFile.path} =\n$serialized", backTrace = false)
-                return null
-            }
-        }
-    }
-}
+   
