660a3e16db128bad26932f51de6d35099568d053
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:17 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/utils/Dirty.kt
+++ app/src/main/java/com/machiav3lli/backup/utils/Dirty.kt
@@ -2,29 +2,29 @@ package com.machiav3lli.backup.utils
 
 import androidx.core.text.isDigitsOnly
 
-// define dirty values and functions
-// e.g. hardcoded, possibly version dependent values
-// not necessarily portable or future-proof assumptions
-// a.k.a. HACKS!
-
-//TODO all these might be improved by using more future-proof strategies
-
+// This class, Dirty, contains a collection of "dirty" values and functions. These are essentially
+// hard-coded, potentially version-dependent values and assumptions that could be improved upon.
+// They are referred to as "dirty" because they are not necessarily future-proof or portable.
 class Dirty {
+    // The companion object for the Dirty class contains a few constants and functions.
     companion object {
+        // These constants represent the first and last application IDs. The assumption here is that
+        // the group ID (GID) is equal to the user ID (UID), which is usually the case.
         val FIRST_APPLICATION_GID = android.os.Process.FIRST_APPLICATION_UID  //TODO ASSUMPTION: gid==uid (usually 10000)
         val LAST_APPLICATION_GID  = android.os.Process.LAST_APPLICATION_UID   //TODO ASSUMPTION: gid==uid (usually 19999)
-        //val FIRST_APPLICATION_CACHE_GID = FIRST_APPLICATION_GID * 2         //TODO ASSUMPTION: *2 or +10000
+
+        // This constant represents the first cache group ID, which is assumed to follow the normal
+        // group of application IDs.
         val FIRST_APPLICATION_CACHE_GID = LAST_APPLICATION_GID+1              //TODO ASSUMPTION: cache group following normal group
 
-        // cache groups seem to be offset by 10000 to the normal app group
+        // This function converts an application group ID to a cache group ID.
         fun appGidToCacheGid(appGid: Int) = appGid - FIRST_APPLICATION_GID + FIRST_APPLICATION_CACHE_GID
 
+        // This function converts an application group ID (either a string or an integer) to a cache
+        // group ID. If the input is a string, it first checks if it's all digits. If it is, it converts
+        // it to an integer and proceeds as before. If it's not all digits, it tries to get the numeric
+        // GID for the name. If the name doesn't correspond to a valid GID, it returns the original name.
         fun appGidToCacheGid(appGid: String): String {
-            // in case the toybox is compiled statically, it usually returns numeric uid/gid
-            // because Glibc uses dlopen to implement nss switching (/etc/nsswitch.conf)
-            // and dynamic linking in static linked binaries restricts the loadable library versions
-            //TODO hg42 could also try to get the name for the gid and add "_cache" to it,
-            //TODO hg42 but I didn't find a num->name function
             return if (appGid.isDigitsOnly()) {
                 val numGid = appGid.toInt()
                 if (numGid < FIRST_APPLICATION_GID || LAST_APPLICATION_GID < numGid) // something special like sdcard_rw
@@ -42,3 +42,4 @@ class Dirty {
         }
     }
 }
+
