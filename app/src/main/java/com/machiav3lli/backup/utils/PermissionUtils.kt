762e446ff5dd13d3c8c5ac497fabbc0e3f5c04d7
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:30 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/utils/PermissionUtils.kt
+++ app/src/main/java/com/machiav3lli/backup/utils/PermissionUtils.kt
@@ -24,50 +24,61 @@ import com.machiav3lli.backup.handler.ShellHandler
 import com.machiav3lli.backup.preferences.persist_ignoreBatteryOptimization
 import com.topjohnwu.superuser.Shell
 
-// Getters
-
+// Check if the device has root access
+// Return true if the device has root access, false otherwise
+// If showDialogOnError is true, show a fatal UI warning when the device doesn't have root access
 fun Activity.checkRootAccess(showDialogOnError: Boolean = false): Boolean {
+    // Get the shell object and check if it has root access
     val isRooted = Shell.getShell().isRoot
     if (!isRooted) {
         if (showDialogOnError)
-            showFatalUiWarning(getString(R.string.noSu))
-        return false
+            showFatalUiWarning(getString(R.string.noSu)) // Show a fatal UI warning
+        return false // Return false if the device doesn't have root access
     }
+    // Check if the app can run commands as root
     try {
-        ShellHandler.runAsRoot("id")
+        ShellHandler.runAsRoot("id") // Run the 'id' command as root
     } catch (e: ShellHandler.ShellCommandFailedException) {
-        showFatalUiWarning(getString(R.string.noSu))
-        return false
+        showFatalUiWarning(getString(R.string.noSu)) // Show a fatal UI warning
+        return false // Return false if the app can't run commands as root
     }
-    return true
+    return true // Return true if the device has root access and the app can run commands as root
 }
 
+// Check if all permissions are granted
+// Return true if all permissions are granted, false otherwise
 val Context.allPermissionsGranted: Boolean
     get() {
+        // Get the PowerManager service
         val powerManager = getSystemService(AppCompatActivity.POWER_SERVICE) as PowerManager
-        return hasStoragePermissions &&
-                isStorageDirSetAndOk &&
-                checkSMSMMSPermission &&
-                checkCallLogsPermission &&
-                checkContactsPermission &&
-                checkUsageStatsPermission &&
-                postNotificationsPermission &&
-                checkBatteryOptimization(powerManager)
+        return hasStoragePermissions && // Check if the storage permissions are granted
+                isStorageDirSetAndOk && // Check if the storage directory is set and ok
+                checkSMSMMSPermission && // Check if the SMS/MMS permissions are granted
+                checkCallLogsPermission && // Check if the call logs permission is granted
+                checkContactsPermission && // Check if the contacts permission is granted
+                checkUsageStatsPermission && // Check if the usage stats permission is granted
+                postNotificationsPermission && // Check if the post notifications permission is granted
+                checkBatteryOptimization(powerManager) // Check if the battery optimization is ignored
     }
 
+// Check if the app has storage permissions
+// Return true if the app has storage permissions, false otherwise
 val Context.hasStoragePermissions: Boolean
     get() = when {
-        OABX.minSDK(Build.VERSION_CODES.R) ->
-            Environment.isExternalStorageManager()
+        OABX.minSDK(Build.VERSION_CODES.R) -> // Check the minimum SDK version
+            Environment.isExternalStorageManager() // Return true if the app has the All files access permission on Android 11 and higher
 
-        else                               ->
-            checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+        else                               -> // For Android 10 and lower
+            checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == // Check if the READ_EXTERNAL_STORAGE permission is granted
                     PackageManager.PERMISSION_GRANTED
     }
 
+// Check if the storage directory is set and ok
+// Return true if the storage directory is set and ok, false otherwise
 val Context.isStorageDirSetAndOk: Boolean
     get() {
         return try {
+            // Get the storage directory path and check if it exists
             val storageDirPath = backupDirConfigured
             if (storageDirPath.isEmpty()) {
                 return false
@@ -80,6 +91,8 @@ val Context.isStorageDirSetAndOk: Boolean
         }
     }
 
+// Check if the app has SMS/MMS permissions
+// Return true if the app has SMS/MMS permissions, false otherwise
 val Context.checkSMSMMSPermission: Boolean
     get() {
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
@@ -108,208 +121,4 @@ val Context.checkSMSMMSPermission: Boolean
                     checkCallingOrSelfPermission(Manifest.permission.RECEIVE_SMS) ==
                     PackageManager.PERMISSION_GRANTED &&
                     checkCallingOrSelfPermission(Manifest.permission.RECEIVE_MMS) ==
-                    PackageManager.PERMISSION_GRANTED &&
-                    checkCallingOrSelfPermission(Manifest.permission.RECEIVE_WAP_PUSH) ==
-                    PackageManager.PERMISSION_GRANTED)
-        } else {
-            mode == AppOpsManager.MODE_ALLOWED
-        }
-    }
-
-
-val Context.checkCallLogsPermission: Boolean
-    get() {
-        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-            return true
-        }
-        if (!specialBackupsEnabled) {
-            return true
-        }
-        val appOps = (getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager)
-        val mode = when {
-            OABX.minSDK(Build.VERSION_CODES.Q) ->
-                appOps.unsafeCheckOpNoThrow(
-                    AppOpsManager.OPSTR_READ_CALL_LOG,
-                    Process.myUid(),
-                    packageName
-                )
-            // Done this way because on (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q)
-            // it always says that the permission is granted even though it is not
-            else                               -> AppOpsManager.MODE_DEFAULT
-        }
-        return if (mode == AppOpsManager.MODE_DEFAULT) {
-            (checkCallingOrSelfPermission(Manifest.permission.READ_CALL_LOG) ==
-                    PackageManager.PERMISSION_GRANTED &&
-                    checkCallingOrSelfPermission(Manifest.permission.WRITE_CALL_LOG) ==
-                    PackageManager.PERMISSION_GRANTED)
-        } else {
-            mode == AppOpsManager.MODE_ALLOWED
-        }
-    }
-
-
-val Context.checkContactsPermission: Boolean
-    get() {
-        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-            return true
-        }
-        if (!specialBackupsEnabled) {
-            return true
-        }
-        val appOps = (getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager)
-        val mode = when {
-            OABX.minSDK(Build.VERSION_CODES.Q) ->
-                appOps.unsafeCheckOpNoThrow(
-                    AppOpsManager.OPSTR_READ_CONTACTS,
-                    Process.myUid(),
-                    packageName
-                )
-            // Done this way because on (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-            // it always says that the permission is granted even though it is not
-            else                               -> AppOpsManager.MODE_DEFAULT
-        }
-        return if (mode == AppOpsManager.MODE_DEFAULT) {
-            checkCallingOrSelfPermission(Manifest.permission.READ_CONTACTS) ==
-                    PackageManager.PERMISSION_GRANTED
-        } else {
-            mode == AppOpsManager.MODE_ALLOWED
-        }
-    }
-
-val Context.checkUsageStatsPermission: Boolean
-    get() {
-        val appOps = (getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager)
-        val mode = when {
-            OABX.minSDK(Build.VERSION_CODES.Q) ->
-                appOps.unsafeCheckOpNoThrow(
-                    AppOpsManager.OPSTR_GET_USAGE_STATS,
-                    Process.myUid(),
-                    packageName
-                )
-
-            else                               ->
-                appOps.checkOpNoThrow(  //TODO 'checkOpNoThrow(String, Int, String): Int' is deprecated. Deprecated in Java. @machiav3lli not replaceable without increasing minSDK as the two functions have different minSDK
-                    AppOpsManager.OPSTR_GET_USAGE_STATS,
-                    Process.myUid(),
-                    packageName
-                )
-        }
-        return if (mode == AppOpsManager.MODE_DEFAULT) {
-            checkCallingOrSelfPermission(Manifest.permission.PACKAGE_USAGE_STATS) ==
-                    PackageManager.PERMISSION_GRANTED
-        } else {
-            mode == AppOpsManager.MODE_ALLOWED
-        }
-    }
-
-val Context.postNotificationsPermission: Boolean
-    get() = if (OABX.minSDK(Build.VERSION_CODES.TIRAMISU)) {
-        checkCallingOrSelfPermission(
-            Manifest.permission.POST_NOTIFICATIONS
-        ) == PackageManager.PERMISSION_GRANTED
-    } else true
-
-fun Context.checkBatteryOptimization(powerManager: PowerManager)
-        : Boolean = persist_ignoreBatteryOptimization.value
-        || powerManager.isIgnoringBatteryOptimizations(packageName)
-
-// Actions
-
-fun Activity.requireStorageLocation(activityResultLauncher: ActivityResultLauncher<Intent>) {
-    val intent = BACKUP_DIRECTORY_INTENT
-    try {
-        activityResultLauncher.launch(intent)
-    } catch (e: ActivityNotFoundException) {
-        showWarning(
-            getString(R.string.no_file_manager_title),
-            getString(R.string.no_file_manager_message)
-        ) { _: DialogInterface?, _: Int ->
-            finishAffinity()
-        }
-    }
-}
-
-
-fun Activity.getStoragePermission() {
-    when {
-        OABX.minSDK(Build.VERSION_CODES.R) -> {
-            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-            intent.data = Uri.parse("package:$packageName")
-            startActivity(intent)
-        }
-
-        else                               -> {
-            requireWriteStoragePermission()
-            requireReadStoragePermission()
-        }
-    }
-}
-
-private fun Activity.requireReadStoragePermission() {
-    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
-        PackageManager.PERMISSION_GRANTED
-    )
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), READ_PERMISSION
-        )
-}
-
-private fun Activity.requireWriteStoragePermission() {
-    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
-        PackageManager.PERMISSION_GRANTED
-    )
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_PERMISSION
-        )
-}
-
-fun Activity.requireSMSMMSPermission() {
-    val smsmmsPermissionList = arrayOf(
-        Manifest.permission.READ_SMS,
-        Manifest.permission.SEND_SMS,
-        Manifest.permission.RECEIVE_SMS,
-        Manifest.permission.RECEIVE_MMS,
-        Manifest.permission.RECEIVE_WAP_PUSH
-    )
-    if (
-        checkSelfPermission(Manifest.permission.READ_SMS) !=
-        PackageManager.PERMISSION_GRANTED ||
-        checkSelfPermission(Manifest.permission.SEND_SMS) !=
-        PackageManager.PERMISSION_GRANTED ||
-        checkSelfPermission(Manifest.permission.RECEIVE_SMS) !=
-        PackageManager.PERMISSION_GRANTED ||
-        checkSelfPermission(Manifest.permission.RECEIVE_MMS) !=
-        PackageManager.PERMISSION_GRANTED ||
-        checkSelfPermission(Manifest.permission.RECEIVE_WAP_PUSH) !=
-        PackageManager.PERMISSION_GRANTED
-    )
-        ActivityCompat.requestPermissions(this, smsmmsPermissionList, SMS_PERMISSION)
-}
-
-fun Activity.requireCallLogsPermission() {
-    val callLogPermissionList = arrayOf(
-        Manifest.permission.READ_CALL_LOG,
-        Manifest.permission.WRITE_CALL_LOG
-    )
-    if (
-        checkSelfPermission(Manifest.permission.READ_CALL_LOG) !=
-        PackageManager.PERMISSION_GRANTED ||
-        checkSelfPermission(Manifest.permission.WRITE_CALL_LOG) !=
-        PackageManager.PERMISSION_GRANTED
-    )
-        ActivityCompat.requestPermissions(this, callLogPermissionList, CALLLOGS_PERMISSION)
-}
-
-fun Activity.requireContactsPermission() {
-    if (
-        checkSelfPermission(Manifest.permission.READ_CONTACTS) !=
-        PackageManager.PERMISSION_GRANTED
-    )
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.READ_CONTACTS),
-            CONTACTS_PERMISSION
-        )
-}
+                    PackageManager.PERMI
