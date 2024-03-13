ecd71b7c0dc0beab560d683d14f88de1fe5b4acc
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:39 +0530

    improve

--- app/src/main/assets/files/package.sh
+++ app/src/main/assets/files/package.sh
@@ -1,112 +1,91 @@
-#!/system/bin/sh
+#!/bin/bash
 
-command=$1
-shift
+# Function to kill all background processes of a package
+kill_package_pids() {
+  local package=$1
+  local userid=$2
 
-utilbox=$1
-shift
-
-suspend=false
-
-while [[ $1 == --* ]]; do
-
-  if [[ $1 == --suspend ]]; then
-    shift
-    suspend=true
-    continue
-  fi
-
-  break
-
-done
-
-
-if [[ $command == "pre-backup" ]]; then
-
-  package=$1
-  shift
-
-  userid=$1
-  shift
-
-
-  #am force-stop $package
-  #am kill $package
-
-  if $suspend; then
-    pm suspend $package >/dev/null
-    $utilbox sleep 3
-  fi
-
-  pids=$(
+  # Get pids of package processes
+  local pids=$(
     (
-      $utilbox ps -A -o PID -u $userid | $utilbox tail -n +2
-      $utilbox ls -l /proc/*/fd/* 2>/dev/null |
-          $utilbox grep -E "/data/data/|/media/" |
-          #$utilbox grep -E "/data/|/media/" |  #TODO add an option for wider catching
-          $utilbox grep -F /$package/ |
-          $utilbox cut -s -d / -f 3
-    ) |
-    $utilbox sort -u -n
+      ps -A -o PID -u $userid | tail -n +2
+      ls -l /proc/*/fd/* 2>/dev/null |
+          grep -E "/data/data/$package/|/media/" |
+          grep -F /$package/ |
+          cut -s -d / -f 3
+    ) | sort -u -n
   )
 
-  #echo "pids=( $pids )"
-
+  # Stop background processes
   if [[ -n $pids ]]; then
-    $utilbox ps -A -w -o USER,PID,NAME -p $pids |
+    ps -A -w -o USER,PID,NAME -p $pids |
       while read -r user pid process; do
-        if [[ $user    != u0_*                        ]]; then continue; fi
-        if [[ $process == android.process.media       ]]; then continue; fi
-        if [[ $process == com.android.externalstorage ]]; then continue; fi
-        $utilbox kill -STOP $pid && echo $pid
+        if [[ $user != u0_* ]]; then continue; fi
+        if [[ $process == android.process.media || $process == com.android.externalstorage ]]; then continue; fi
+        kill -STOP $pid && echo $pid
       done
   fi
+}
 
-  exit
+# Parse command line arguments
+while getopts "c:u:s" opt; do
+  case $opt in
+    c)
+      command=$OPTARG
+      ;;
+    u)
+      userid=$OPTARG
+      ;;
+    s)
+      suspend=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Check if command is valid
+if [[ -z $command ]]; then
+  echo "Invalid command. Use -c <command> where command is one of pre-backup, post-backup, pre-restore, or post-restore." >&2
+  exit 1
 fi
 
-if [[ $command == "post-backup" ]]; then
-
-  package=$1
-  shift
-
-  userid=$1
-  shift
-
-  # kill background processes, which should be restarted by the system when necessary
-  am kill $package
-
-  if [[ -n $* ]]; then $utilbox kill -CONT "$@"; fi
-
-  if $suspend; then
-    pm unsuspend $package
-  fi
-
-  exit
+# Check if userid is valid
+if ! id -u -r "$userid" >/dev/null 2>&1; then
+  echo "Invalid userid. Userid must be a valid user id." >&2
+  exit 1
 fi
 
-if [[ $command == "pre-restore" ]]; then
+package=$1
+shift
 
-  package=$1
-  shift
+case $command in
+  pre-backup)
+    # Stop package processes
+    if $suspend; then
+      pm suspend "$package" >/dev/null
+      sleep 3
+    fi
+    kill_package_pids "$package" "$userid"
+    ;;
+  post-backup)
+    # Kill package processes
+    am kill "$package"
 
-  userid=$1
-  shift
+    # Restart background processes
+    if [[ -n $* ]]; then
+      kill -CONT "$@"
+    fi
 
-  # kill app and all of its services without canceling it's scheduled alarms and jobs.
-  # if command does not exist (or fails) stop everything instead
-  am stop-app $package || am force-stop $package
-
-  exit
-fi
-
-if [[ $command == "post-restore" ]]; then
-
-  package=$1
-  shift
-
-  userid=$1
-  shift
-
-  exit
-fi
+    if $suspend; then
+      pm unsuspend "$package"
+    fi
+    ;;
+  pre-restore)
+    # Stop app
