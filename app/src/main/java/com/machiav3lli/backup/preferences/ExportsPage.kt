15317f1bc24c5aea22a5b99183825812da6a5175
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:40 +0530

    improve

--- app/src/main/java/com/machiav3lli/backup/preferences/ExportsPage.kt
+++ app/src/main/java/com/machiav3lli/backup/preferences/ExportsPage.kt
@@ -15,38 +15,61 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.machiav3lli.backup.preferences
 
-import android.annotation.SuppressLint
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.machiav3lli.backup.R
+import com.machiav3lli.backup.databinding.ActivityMainBinding
 import com.machiav3lli.backup.ui.compose.blockBorder
 import com.machiav3lli.backup.ui.compose.icons.Phosphor
-import com.machiav3lli.backup.ui.compose.icons.phosphor.CalendarPlus
 import com.machiav3lli.backup.ui.compose.item.TopBar
 import com.machiav3lli.backup.ui.compose.recycler.ExportedScheduleRecycler
 import com.machiav3lli.backup.ui.navigation.NavItem
 import com.machiav3lli.backup.viewmodels.ExportsViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ExportsPage(viewModel: ExportsViewModel) {
+fun ExportsPage(
+    activity: ComponentActivity,
+    snackbarHostState: SnackbarHostState,
+    viewModel: ExportsViewModel,
+    initialization: @Composable () -> Unit
+) {
     val exports by viewModel.exportsList.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
 
-    SideEffect {
-        viewModel.refreshList()
+    DisposableEffect(viewModel) {
+        coroutineScope.launch(Dispatchers.IO) {
+            viewModel.refreshList()
+        }
+
+        onDispose {
+            viewModel.onDispose()
+        }
     }
 
     Scaffold(
@@ -61,17 +84,17 @@ fun ExportsPage(viewModel: ExportsViewModel) {
             }
         },
         topBar = {
-            TopBar(title = stringResource(id = NavItem.Exports.title))
-        }
-    ) { paddingValues ->
-        ExportedScheduleRecycler(
-            modifier = Modifier
-                .padding(paddingValues)
-                .blockBorder()
-                .fillMaxSize(),
-            productsList = exports,
-            onImport = { viewModel.importSchedule(it) },
-            onDelete = { viewModel.deleteExport(it) }
-        )
-    }
-}
+            TopAppBar(
+                title = { Text(text = stringResource(id = NavItem.Exports.title)) },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent)
+            )
+        },
+        snackbarHost = snackbarHostState,
+        content = initialization
+    )
+
+    LaunchedEffect(exports) {
+        if (exports.isEmpty()) {
+            snackbarHostState.showSnackbar(
+                message = stringResource(id = R.string.no_exported_schedules),
+                actionLabel = stringResource(id = R
