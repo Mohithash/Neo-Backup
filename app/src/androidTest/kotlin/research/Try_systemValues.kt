0e6c0a7f47eccaffc4ce0e724c329dd09476ee65
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:29 +0530

    improve

--- app/src/androidTest/kotlin/research/Try_systemValues.kt
+++ app/src/androidTest/kotlin/research/Try_systemValues.kt
@@ -4,7 +4,21 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import timber.log.Timber
 
-class Try_systemValues {
+class SystemValuesTest {
 
     @Test
+    fun testSystemValues() {
+        Timber.plant(Timber.DebugTree())
 
+        val systemProperty = "os.name"
+        val expectedValue = "Linux"
+        val actualValue = System.getProperty(systemProperty)
+
+        Timber.d("System property $systemProperty has value $actualValue")
+        assertEquals(expectedValue, actualValue)
+
+        val environmentVariable = "PATH"
+        val expectedEnvironmentValue = System.getenv(environmentVariable)
+        Timber.d("Environment variable $environmentVariable has value $expectedEnvironmentValue")
+    }
+}
