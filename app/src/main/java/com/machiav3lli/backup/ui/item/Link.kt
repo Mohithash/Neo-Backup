318d63bc33eefb11300242c7ec99c474ea76aeda
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:14 +0530

    comment

--- app/src/main/java/com/machiav3lli/backup/ui/item/Link.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/item/Link.kt
@@ -1,5 +1,7 @@
+// Package for the UI-related components of the backup application.
 package com.machiav3lli.backup.ui.item
 
+// Import statements for required classes and resources.
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.machiav3lli.backup.HELP_CHANGELOG
 import com.machiav3lli.backup.HELP_FAQ
@@ -16,49 +18,54 @@ import com.machiav3lli.backup.ui.compose.icons.phosphor.Info
 import com.machiav3lli.backup.ui.compose.icons.phosphor.TelegramLogo
 import com.machiav3lli.backup.ui.compose.icons.phosphor.Warning
 
+// Data class representing a link with a name, icon, icon color, and URI.
 data class Link(
+
+    // Name ID for the link, used to display the name in the application.
     val nameId: Int,
+
+    // Image vector for the link's icon.
     val icon: ImageVector,
+
+    // Icon color ID for the link's icon.
     val iconColorId: Int,
+
+    // URI for the link, used to open the corresponding webpage or resource.
     val uri: String,
 ) {
 
+    // Companion object containing predefined Link instances for various help resources.
     companion object {
+        // Changelog link to the application's change log.
         val Changelog = Link(
             R.string.help_changelog,
             Phosphor.ArrowsClockwise,
             R.color.ic_updated,
             HELP_CHANGELOG
         )
+
+        // Telegram link to the application's Telegram group.
         val Telegram = Link(
             R.string.help_group_telegram,
             Phosphor.TelegramLogo,
             R.color.ic_system,
             HELP_TELEGRAM
         )
+
+        // Matrix link to the application's Matrix group.
         val Matrix = Link(
             R.string.help_group_matrix,
             Phosphor.BracketsSquare,
             R.color.ic_apk,
             HELP_MATRIX
         )
+
+        // License link to the application's license information.
         val License = Link(
             R.string.help_license,
             Phosphor.Info,
             R.color.ic_ext_data,
             HELP_LICENSE
         )
-        val Issues = Link(
-            R.string.help_issues,
-            Phosphor.Warning,
-            R.color.ic_de_data,
-            HELP_ISSUES
-        )
-        val FAQ = Link(
-            R.string.help_faq,
-            Phosphor.CircleWavyQuestion,
-            R.color.ic_special,
-            HELP_FAQ
-        )
-    }
-}
+
+        // Issues link to the application
