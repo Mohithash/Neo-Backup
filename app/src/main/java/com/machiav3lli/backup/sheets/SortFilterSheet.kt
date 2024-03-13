7ef51bad28ac7c6940436b4a9cfe811b91e4a84d
Mohithash <shamnadanu927@gmail.com>
2024-03-13 22:59:43 +0530

    improve

--- app/src/main/java/com/machiav3lli/backup/sheets/SortFilterSheet.kt
+++ app/src/main/java/com/machiav3lli/backup/sheets/SortFilterSheet.kt
@@ -15,25 +15,39 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.machiav3lli.backup.sheets
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -41,9 +55,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.machiav3lli.backup.OABX
 import com.machiav3lli.backup.R
 import com.machiav3lli.backup.activities.MainActivityX
@@ -64,194 +83,19 @@ import com.machiav3lli.backup.ui.compose.icons.phosphor.SortAscending
 import com.machiav3lli.backup.ui.compose.icons.phosphor.SortDescending
 import com.machiav3lli.backup.ui.compose.item.CategoryTitleText
 import com.machiav3lli.backup.ui.compose.item.DoubleVerticalText
-import com.machiav3lli.backup.ui.compose.item.ElevatedActionButton
-import com.machiav3lli.backup.ui.compose.item.RoundButton
 import com.machiav3lli.backup.ui.compose.item.SwitchChip
 import com.machiav3lli.backup.ui.compose.recycler.MultiSelectableChipGroup
 import com.machiav3lli.backup.ui.compose.recycler.SelectableChipGroup
-import com.machiav3lli.backup.ui.item.ChipItem
 import com.machiav3lli.backup.updatedFilterChipItems
 import com.machiav3lli.backup.utils.applyFilter
 import com.machiav3lli.backup.utils.getStats
 import com.machiav3lli.backup.utils.sortFilterModel
 import com.machiav3lli.backup.utils.specialBackupsEnabled
+import kotlinx.coroutines.launch
 
 @Composable
-fun SortFilterSheet(onDismiss: () -> Unit) {
+fun SortFilterSheet(onDismiss: () -> Unit, activity: ComponentActivity) {
     val context = LocalContext.current
-    val mActivity = context as MainActivityX
+    val mActivity = activity
     val nestedScrollConnection = rememberNestedScrollInteropConnection()
-    val packageList by mActivity.viewModel.notBlockedList.collectAsState()
-    var model by rememberSaveable { mutableStateOf(sortFilterModel) }
-    fun currentStats() = getStats(
-        packageList.applyFilter(
-            model,
-            OABX.context,
-        )
-    )  //TODO hg42 use central function for all the filtering
-
-    Scaffold(
-        containerColor = Color.Transparent,
-        contentColor = MaterialTheme.colorScheme.onSurface,
-        topBar = {
-            ListItem(
-                colors = ListItemDefaults.colors(
-                    containerColor = Color.Transparent,
-                ),
-                headlineContent = {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        DoubleVerticalText(
-                            upperText = currentStats().first.toString(),
-                            bottomText = stringResource(id = R.string.stats_apps),
-                            modifier = Modifier.weight(1f)
-                        )
-                        DoubleVerticalText(
-                            upperText = currentStats().second.toString(),
-                            bottomText = stringResource(id = R.string.stats_backups),
-                            modifier = Modifier.weight(1f)
-                        )
-                        DoubleVerticalText(
-                            upperText = currentStats().third.toString(),
-                            bottomText = stringResource(id = R.string.stats_updated),
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                },
-                trailingContent = {
-                    RoundButton(icon = Phosphor.CaretDown) {
-                        onDismiss()
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            Row(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .navigationBarsPadding(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                ElevatedActionButton(
-                    text = stringResource(id = R.string.resetFilter),
-                    icon = Phosphor.ArrowUUpLeft,
-                    modifier = Modifier.weight(1f),
-                    fullWidth = true,
-                    positive = false,
-                    onClick = {
-                        sortFilterModel = SortFilterModel()
-                        onDismiss()
-                    }
-                )
-                ElevatedActionButton(
-                    text = stringResource(id = R.string.applyFilter),
-                    icon = Phosphor.Check,
-                    modifier = Modifier.weight(1f),
-                    fullWidth = true,
-                    positive = true,
-                    onClick = {
-                        sortFilterModel = model
-                        onDismiss()
-                    }
-                )
-            }
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .padding(paddingValues)
-                .blockBorder()
-                .nestedScroll(nestedScrollConnection)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(8.dp)
-        ) {
-            item { CategoryTitleText(R.string.sorting_order) }
-            item {
-                SelectableChipGroup(
-                    list = sortChipItems,
-                    selectedFlag = model.sort
-                ) { flag ->
-                    model = model.copy(sort = flag)
-                }
-                SwitchChip(
-                    firstTextId = R.string.sortAsc,
-                    firstIcon = Phosphor.SortAscending,
-                    secondTextId = R.string.sortDesc,
-                    secondIcon = Phosphor.SortDescending,
-                    firstSelected = model.sortAsc,
-                    onCheckedChange = { checked ->
-                        model = model.copy(sortAsc = checked)
-                    }
-                )
-            }
-            item { CategoryTitleText(R.string.filters_app) }
-            item {
-                MultiSelectableChipGroup(
-                    list = if (specialBackupsEnabled) mainFilterChipItems
-                    else mainFilterChipItems.minus(ChipItem.Special),
-                    selectedFlags = model.mainFilter
-                ) { flags, _ ->
-                    model = model.copy(mainFilter = flags)
-                }
-            }
-            item { CategoryTitleText(R.string.filters_backup) }
-            item {
-                MultiSelectableChipGroup(
-                    list = mainBackupModeChipItems,
-                    selectedFlags = model.backupFilter
-                ) { flags, _ ->
-                    model = model.copy(backupFilter = flags)
-                }
-            }
-            item { CategoryTitleText(R.string.filters_installed) }
-            item {
-                SelectableChipGroup(
-                    list = installedFilterChipItems,
-                    selectedFlag = model.installedFilter
-                ) { flag ->
-                    model = model.copy(installedFilter = flag)
-                }
-            }
-            item { CategoryTitleText(R.string.filters_launchable) }
-            item {
-                SelectableChipGroup(
-                    list = launchableFilterChipItems,
-                    selectedFlag = model.launchableFilter
-                ) { flag ->
-                    model = model.copy(launchableFilter = flag)
-                }
-            }
-            item { CategoryTitleText(R.string.filters_updated) }
-            item {
-                SelectableChipGroup(
-                    list = updatedFilterChipItems,
-                    selectedFlag = model.updatedFilter
-                ) { flag ->
-                    model = model.copy(updatedFilter = flag)
-                }
-            }
-            item { CategoryTitleText(R.string.filters_latest) }
-            item {
-                SelectableChipGroup(
-                    list = latestFilterChipItems,
-                    selectedFlag = model.latestFilter
-                ) { flag ->
-                    model = model.copy(latestFilter = flag)
-                }
-            }
-            item { CategoryTitleText(R.string.filters_enabled) }
-            item {
-                SelectableChipGroup(
-                    list = enabledFilterChipItems,
-                    selectedFlag = model.enabledFilter
-                ) { flag ->
-                    model = model.copy(enabledFilter = flag)
-                }
-            }
-        }
-    }
-}
+    val packageList by mActivity.viewModel.notBlocked
